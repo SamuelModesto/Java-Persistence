e98b681bf1c9d902fc8331974b0f1627e0a82762
Samuel Modesto <samuelmodestoes@gmail.com>
2022-10-10 22:37:55 -0300

    JPQL e Native Query

--- src/main/java/com/samuel/estudojpa/service/RelatoriosService.java
+++ src/main/java/com/samuel/estudojpa/service/RelatoriosService.java
@@ -5,6 +5,8 @@ import com.samuel.estudojpa.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,6 +14,7 @@ import java.util.Scanner;
 public class RelatoriosService {
 
     private boolean system = true;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
@@ -21,7 +24,8 @@ public class RelatoriosService {
             System.out.println("Qual ação de cargo deseja executar? ");
             System.out.println("0 - Sair");
             System.out.println("1 - Pesquisar funcionario pelo nome");
-
+            System.out.println("2 - Pesquisar funcionario por filtros");
+            System.out.println("3 - Pesquisar funcionario pela data de contratacao");
 
             int acao = scanner.nextInt();
 
@@ -29,16 +33,44 @@ public class RelatoriosService {
                 case 1:
                     buscarFuncionarioPeloNome(scanner);
                     break;
+                case 2:
+                    buscarFuncionarioPorFiltros(scanner);
+                    break;
+                case 3:
+                    buscarFuncionarioPelaDataDeContratacao(scanner);
+                    break;
                 default:
                     system = false;
             }
         }
     }
 
-    public void buscarFuncionarioPeloNome(Scanner scanner){
+    public void buscarFuncionarioPeloNome(Scanner scanner) {
         System.out.println("Digite o nome que deseja pesquisar: ");
         String nome = scanner.next();
         List<Funcionario> funcionarios = funcionarioRepository.findByNome(nome);
+        funcionarios.forEach(System.out::println);
+    }
+
+    public void buscarFuncionarioPorFiltros(Scanner scanner) {
+        System.out.println("Qual o nome do funcionário que deseja pesquisar: ");
+        String nome = scanner.next();
+        System.out.println("Qual salário deseja pesquisar: ");
+        Double salario = scanner.nextDouble();
+        System.out.println("Qual a data de contratacao: ");
+        String data = scanner.next();
+        LocalDate localDate = LocalDate.parse(data, formatter);
+
+        List<Funcionario> funcionarios = funcionarioRepository.findByNomeDataContratacaoSalario(nome, salario, localDate);
+        funcionarios.forEach(System.out::println);
+    }
+
+    public void buscarFuncionarioPelaDataDeContratacao(Scanner scanner) {
+        System.out.println("Qual a data de contratacao: ");
+        String data = scanner.next();
+        LocalDate localDate = LocalDate.parse(data, formatter);
+
+        List<Funcionario> funcionarios = funcionarioRepository.findDataContratacao(localDate);
         funcionarios.forEach(System.out::println);
     }
 
